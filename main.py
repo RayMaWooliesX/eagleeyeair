b25aaa702588ebc1e44286692f9353339a51da6a
RayMaWooliesX <75294321+RayMaWooliesX@users.noreply.github.com>
2021-07-14 11:37:47 +1000

    Develop (#14)

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ from datetime import datetime, timezone
 import pytz
 import json
 import logging
+import traceback
 import requests
-from pymongo import MongoClient
 import pymongo
+from pymongo import MongoClient
 from google.cloud import pubsub_v1
 from google.cloud import error_reporting
+from flask import request
 
 def main(request):
     """Background Cloud Function to be triggered by Pub/Sub.
@@ -29,61 +31,84 @@ def main(request):
         metadata. The `event_id` field contains the Pub/Sub message ID. The
         `timestamp` field contains the publish time.
     """
-    response_code = '200'
+    # response_code = '200'
+    error_client = error_reporting.Client()
+    
     try:
-        client = error_reporting.Client()
-
-        envelope = json.loads(request.data.decode('utf-8'))
-        print(envelope)
-
-        message = envelope['message']
-
+        print("Preparing data for EE request.")
         url = os.environ['ee_api_url']
         authClientId = os.environ['ee_api_user']
         password = os.environ['ee_api_password']
 
-        event_data = json.loads(base64.b64decode(message["data"]))
-
+        envelope = request.get_json()
+        message = envelope['message']
+        delivery_attempt = envelope['deliveryAttempt']
+        event_data_str = base64.b64decode(message["data"])
+        event_data = json.loads(event_data_str)
         crn = event_data['crn']
-        correlationId = event_data['correlationId']
+
+        event_type = event_data["eventType"]
+        if event_type != 'Change Preference':
+            raise Exception("Incorrect event type!")
+        event_sub_type = event_data["eventSubType"]
+
+        if event_sub_type == "No liquor Offers":
+            preference_label = "noLiquorOffers"
+        elif event_sub_type == "Redemption Setting":
+            preference_label = "redemptionSetting"
+        else:
+            raise Exception("Incorrect sub event type!")
+        
         preferences = event_data['preferences']
-        redemptionSetting = preferences['value']
+        preference_value = preferences['value']
 
-        wallet_id = _get_wallet_id_by_crn(url, authClientId, password, crn, correlationId)['walletId']
-        consumer_id = _get_consumer_id_by_wallet(url, authClientId, password, wallet_id, correlationId)['consumerId']
+        print("Data preparation completed.")
 
-        ##  3. update consumer with new redemption setting
-        update_response = _update_consumer_objects_by_wallet_and_consumer_id(url, authClientId, password, wallet_id, consumer_id, redemptionSetting, correlationId)
-        print('updating completed')
-
+        print("Calling EE APIs to update consumer preference.")
+        wallet_id = _get_wallet_id_by_crn(url, authClientId, password, crn, event_data['correlationId'])
+        consumer_id = _get_consumer_id_by_wallet(url, authClientId, password, wallet_id, event_data['correlationId'])
+        update_response = _update_consumer_objects_by_wallet_and_consumer_id(url, authClientId, password, wallet_id, consumer_id, preference_label, preference_value,  event_data['correlationId'])
+        print('Updating completed.')
         response_code = '200'
 
-    # unacknowledge the message and retry from the pubsub again for a timeout error and log in the mongodb in the last retry
-    except requests.Timeout as e:
-        print("-----Timeout error-------")
-        response_code = 429
-        if message.delivery_attempt == 5:
-            _logging_in_mongodb( correlationId, e.response.status_code, e.response.reason, message.delivery_attempt)
-        logging.error("correlationId: " + correlationId + "; " + e.response.status_code + ": " + e.response.reason)
-        client.report_exception()
-    # forward data errors to dead letter and log in mongodb without retry by ack the message
+        try:
+            _logging_in_mongodb(event_data['correlationId'], '200', 'OK', delivery_attempt)
+        except Exception as e:
+            logging.error(RuntimeError("!!! There was an error while logging in mongodb."))
+            print(traceback.format_exc())
+            error_client.report_exception()
+            pass
+
     except requests.exceptions.RequestException as e:
-        print("-----Request Error-------")
-        _logging_in_mongodb( correlationId, e.response.status_code, e.response.reason, 0)
-        _logging_in_deadletter(event_data, e.response.reason)
-        logging.error("correlationId: " + correlationId + "; " + e.response.status_code + ": " + e.response.reason)
-        response_code = 200
-        client.report_exception()
+        if e.response.status_code == 429:
+            response_code = '500'
+            logging.error(RuntimeError("Too many requests error"))
+            print(traceback.format_exc())
+            error_client.report_exception()
+            if message.delivery_attempt == 5:
+                _logging_in_mongodb( event_data['correlationId'], str(e.response.status_code), e.response.reason + ": " + e.response.text , delivery_attempt)
+            print("Too many requests error logging completed.")
+        else:
+            response_code = '102'
+            logging.error(RuntimeError("Http error: "))
+            print(traceback.format_exc())
+            error_client.report_exception()
+            _logging_in_deadletter(event_data_str.decode('utf-8'), e.response.reason)
+            _logging_in_mongodb( event_data['correlationId'], str(e.response.status_code), e.response.reason + ": " + e.response.text , delivery_attempt)
+            print("Http error logging completed.")
+
     except Exception as e:
-        print("-----Other Error-------")
-        _logging_in_mongodb( correlationId, '000', e.message, 0)
-        _logging_in_deadletter(event_data, e.message)
-        logging.error("correlationId: " + correlationId + "; " + e.message)
-        response_code = 200
-        client.report_exception()
+        response_code = '204'
+        logging.error(RuntimeError('Data error:'))
+        error_msg = ','.join(e.args) + "," + e.__doc__
+        logging.error(RuntimeError(error_msg))
+        print(traceback.format_exc())
+        error_client.report_exception()
+        _logging_in_deadletter(event_data_str.decode('utf-8'), error_msg)
+        if event_data['correlationId']:
+            _logging_in_mongodb( event_data['correlationId'], '400', error_msg, delivery_attempt)
     finally:
         return response_code
-
 
 def _get_wallet_id_by_crn(url, authClientId, password, crn, correlationId):
     '''
@@ -91,11 +116,11 @@ def _get_wallet_id_by_crn(url, authClientId, password, crn, correlationId):
     :param crn:
     :return: Wallet ID  will be returned based on the CRN
     '''
-    service_path = os.environ['ee_get_wallet_service_path'].replace('{{idenitityValueCRN}}', crn)
+    service_path = os.environ['ee_get_wallet_service_path'].replace('{{identityValueCRN}}', crn)
     payload = ''
     headers = _get_header(url, service_path, payload, authClientId, password)
     end_point = url + service_path
-    return _calling_the_request_consumer_object_function("GET", end_point, headers, payload, correlationId)
+    return _calling_the_request_consumer_object_function("GET", end_point, headers, payload, correlationId)['walletId']
 
 def _get_consumer_id_by_wallet(url, authClientId, password, wallet_id, correlationId):
     # This function will be called to get Consumer ID based on Wallet ID
@@ -103,20 +128,19 @@ def _get_consumer_id_by_wallet(url, authClientId, password, wallet_id, correlati
     payload = ''
     end_point = url + service_path
     headers = _get_header(url, service_path, payload, authClientId, password)
-    return _calling_the_request_consumer_object_function("GET", end_point, headers, payload, correlationId)
+    return _calling_the_request_consumer_object_function("GET", end_point, headers, payload, correlationId)['consumerId']
 
-def _update_consumer_objects_by_wallet_and_consumer_id(url, authClientId, password, wallet_id, consumer_id, redemptionSetting, correlationId):
+def _update_consumer_objects_by_wallet_and_consumer_id(url, authClientId, password, wallet_id, consumer_id, preference_label, preference_value, correlationId):
     # This function will be called to update the Consumer objects based on Wallet and Consumer ID
     service_path = update_consumer_service_path = os.environ['ee_update_consumer_service_path'].replace('{{walletId}}', wallet_id).replace('{{consumerId}}', consumer_id)
 
     payload = json.dumps({"friendlyName": "Consumer Details",
                           "data": {"dimension": [
-                                                 {"label": "redemptionSetting",
-                                                  "value": redemptionSetting}
+                                                 {"label": preference_label,
+                                                  "value": preference_value}
                                                 ]
                                    }
                           })
-
     end_point = url + service_path
     headers = _get_header(url, service_path, payload, authClientId, password)
 
@@ -124,26 +148,23 @@ def _update_consumer_objects_by_wallet_and_consumer_id(url, authClientId, passwo
 
 def _calling_the_request_consumer_object_function(mode, end_point, headers, payload, correlationId):
     '''
-    This fucntion will make a call to Eagle Eye API's
+    This function will make a call to Eagle Eye API
     :param mode: will determine if it is get, Post or Patch
     :param end_point: End point is the complete URL
     :param header:  Header is the request header
     :param payload: payload is the request payload
     :return: This function will return the response or return Null
     '''
-
     response = requests.request(mode, end_point, headers=headers, data=payload)
 
     if response.status_code == 200:
         return response.json()
     else:
-        logging.error(response.json())
         response.raise_for_status()
-        return 'NULL'
 
 def _get_header(url, service_path, payload, authClientId, password):
     '''
-    This funtion will prepare the header based on the parameters
+    This function will prepare the header based on the parameters
     :param url:
     :param service_path:
     :param transaction_id:
@@ -160,28 +181,43 @@ def _get_header(url, service_path, payload, authClientId, password):
               "X-RETRY": "1", "X-EES-OPERATOR": "1", "X-EES-CALLER": "DASHBOARD",
               "X-EES-CALLER-VERSION": "0",
               "Content-Type": "application/json"}
+
     return header
 
 def _logging_in_mongodb(correlationId, status_code, status_message, retried_count):
-    url = os.environ['mongodb_url']
-    dbname = os.environ['mongodb_dbname']
-    collection = os.environ['mongodb_collection']
-    changes_updated = 'false' if status_code >= 300 else 'ture'
-    status_object = {"name": "EagleEye", "changesUpdated": changes_updated, "response": {"statusCode": status_code, "message": status_message}, "retriedCount": retried_count, "updatedAt": datetime.now().astimezone(pytz.timezone("Australia/Sydney")).strftime("%Y%m%d-%H%M%S")}
+    print("---Logging in mongodb")
+    try:
+        url = os.environ['mongodb_url']
+        dbname = os.environ['mongodb_dbname']
+        collection = os.environ['mongodb_collection']
+        changes_updated = 'false' if status_code >= '300' else 'true'
+        status_object = {"name": "EagleEye", "changesUpdated": changes_updated, "response": {"statusCode": status_code, "message": status_message}, "retriedCount": retried_count, "updatedAt": datetime.now().astimezone(pytz.timezone("Australia/Sydney")).strftime("%Y%m%d-%H%M%S")}
+        client = pymongo.MongoClient(url)
+        db = client[dbname]
+        col = db[collection]
+        results = col.update_one({'correlationId': correlationId}, {'$push': {'status': status_object}})
 
-    client = MongoClient(url)
-    db = client[dbname]
-    col = db[collection]
-    results = col.update_one({'correlationId': correlationId}, {'$push': {'status': status_object}})
+        print("---Logging in mongodb completed, " + str(results.modified_count) + " records logged.")
+    except Exception as e:
+        logging.error(RuntimeError("!!! There was an error while logging in mongodb."))
+        print(traceback.format_exc())
+        pass
 
 def _logging_in_deadletter(event_data, error_message):
+    try:
+        print("---Logging original message to dead letter topic.")
         error_publisher_client = pubsub_v1.PublisherClient()
         error_topic_path = error_publisher_client.topic_path(os.environ['GCP_PROJECT'], 
                                                             os.environ['error_topic'])
         user = os.environ['FUNCTION_NAME']
-        future = error_publisher_client.publish(error_topic_path, base64.b64decode(event_data) ,
+        future = error_publisher_client.publish(error_topic_path, event_data.encode("utf-8") ,
                                                                         user=user,
                                                                         error = error_message)
         # Wait for the publish future to resolve before exiting.
         while not future.done():
-            time.sleep(5)
+            time.sleep(1)
+        print("---Logging original message to dead letter topic completed.")
+    except Exception as e:
+        logging.error(RuntimeError("!!! There was an error while sending error message to dead letter topic. " ))
+        print(traceback.format_exc())
+        pass
---
+++ test_main.py
@@ -0,0 +1,181 @@
+import unittest
+from unittest import mock
+import os
+from requests.api import patch
+from requests.models import Response
+import main
+import base64
+import hashlib
+import time
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from flask import Flask
+import requests
+
+@mock.patch("main._get_wallet_id_by_crn", return_value=123)
+@mock.patch("main._get_consumer_id_by_wallet", return_value=123)
+@mock.patch("main._update_consumer_objects_by_wallet_and_consumer_id")
+@mock.patch("main._logging_in_mongodb")
+# @mock.patch("main._logging_in_deadletter")
+def test_main(mock_get_wallet, mock_get_consumerId, mock_update_consumer, mock_log_mongo):
+    os.environ["ee_api_url"] = "ee_api_url"
+    os.environ["ee_api_user"] = "ee_api_user"
+    os.environ["ee_api_password"] = "ee_api_password"
+
+    name = "test"
+    data = {"deliveryAttempt": 1, "message": {"data": "eyJzb3VyY2UiOnsiY29kZSI6MSwibmFtZSI6IkNQT1JUQUwifSwidHJhY2tpbmdJZCI6ImM2Mzk4MDExLWNlMDYtNGVlOC1iZGM3LWY1MzE5Zjc3MTJhNyIsImV2ZW50VHlwZSI6IkNoYW5nZSBQcmVmZXJlbmNlIiwgImV2ZW50U3ViVHlwZSI6Ik5vIGxpcXVvciBPZmZlcnMiLCJwdWJsaXNoZWRBdCI6IjIwMjEtMDUtMTkwMzo1ODoxNy4yNDciLCJjcm4iOiIzZmU4Y2RmMjRmNGJiMTNkNWQ1NmM5NGY2ZjUyMjkwYSIsImhhc2hlZENybiI6IjM1NWMzMTAwZDUzNmVhNGYyZThiYjFmNjQ3YjMwM2U3YWE2MjJmMDU2MGVmYzRiNzk4ODQ2MzQwNmYzYjM1OWIiLCJjYXJkTnVtYmVyIjoiOTM1NTA0OTM3OTMyOSIsImNvcnJlbGF0aW9uSWQiOiJjNjM5ODAxMS1jZTA2LTRlZTgtYmRjNy1mNTMxOWY3NzEyYTciLCJwcmVmZXJlbmNlcyI6eyJpZCI6IjEwNDIiLCJ2YWx1ZSI6ZmFsc2V9fQ==", "messageId": "2652670209994957", "message_id": "2652670209994957", "publishTime": "2021-07-11T10:26:03.697Z", "publish_time": "2021-07-11T10:26:03.697Z"}, "subscription": "projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub"}
+    req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
+    
+    main.main(req)
+
+    mock_get_wallet.assert_called_once()
+    mock_get_consumerId.assert_called_once()
+    mock_update_consumer.assert_called_once()
+    mock_log_mongo.assert_called_once()
+    
+    assert main.main(req) == "200"
+
+
+
+@mock.patch("main._logging_in_deadletter")
+@mock.patch("google.cloud.error_reporting.Client.report_exception", return_value= "error reporting")
+def test_main_data_error(mock_log_deadletter, mock_error_reporting):
+    os.environ["ee_api_url"] = "ee_api_url"
+    os.environ["ee_api_user"] = "ee_api_user"
+    os.environ["ee_api_password"] = "ee_api_password"
+
+    name = "test"
+    data = '{"deliveryAttempt": 1, "message": {"dataa": "eyJzb3VyY2UiOnsiY29kZSI6MSwibmFtZSI6IkNQT1JUQUwifSwidHJhY2tpbmdJZCI6ImM2Mzk4MDExLWNlMDYtNGVlOC1iZGM3LWY1MzE5Zjc3MTJhNyIsImV2ZW50VHlwZSI6IkNoYW5nZSBQcmVmZXJlbmNlIiwgImV2ZW50U3ViVHlwZSI6Ik5vIGxpcXVvciBPZmZlcnMiLCJwdWJsaXNoZWRBdCI6IjIwMjEtMDUtMTkwMzo1ODoxNy4yNDciLCJjcm4iOiIzZmU4Y2RmMjRmNGJiMTNkNWQ1NmM5NGY2ZjUyMjkwYSIsImhhc2hlZENybiI6IjM1NWMzMTAwZDUzNmVhNGYyZThiYjFmNjQ3YjMwM2U3YWE2MjJmMDU2MGVmYzRiNzk4ODQ2MzQwNmYzYjM1OWIiLCJjYXJkTnVtYmVyIjoiOTM1NTA0OTM3OTMyOSIsImNvcnJlbGF0aW9uSWQiOiJjNjM5ODAxMS1jZTA2LTRlZTgtYmRjNy1mNTMxOWY3NzEyYTciLCJwcmVmZXJlbmNlcyI6eyJpZCI6IjEwNDIiLCJ2YWx1ZSI6ZmFsc2V9fQ", "messageId": "2652670209994957", "message_id": "2652670209994957", "publishTime": "2021-07-11T10:26:03.697Z", "publish_time": "2021-07-11T10:26:03.697Z"}, "subscription": "projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub"}'
+    req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
+    
+    main.main(req)   
+    
+    assert main.main(req) == "204"
+
+
+
+@mock.patch("main._get_wallet_id_by_crn", autospec=True)
+@mock.patch("main._logging_in_mongodb")
+@mock.patch("main._logging_in_deadletter")
+@mock.patch("builtins.print")
+def test_main_429(mock_get_wallet, mock_log_mongo, mock_deadletter, mock_print):
+    os.environ["ee_api_url"] = "ee_api_url"
+    os.environ["ee_api_user"] = "ee_api_user"
+    os.environ["ee_api_password"] = "ee_api_password"
+
+    name = "test"
+    data = '{"deliveryAttempt": 1, "message": {"data": "eyJzb3VyY2UiOnsiY29kZSI6MSwibmFtZSI6IkNQT1JUQUwifSwidHJhY2tpbmdJZCI6ImM2Mzk4MDExLWNlMDYtNGVlOC1iZGM3LWY1MzE5Zjc3MTJhNyIsImV2ZW50VHlwZSI6IkNoYW5nZSBQcmVmZXJlbmNlIiwgImV2ZW50U3ViVHlwZSI6Ik5vIGxpcXVvciBPZmZlcnMiLCJwdWJsaXNoZWRBdCI6IjIwMjEtMDUtMTkwMzo1ODoxNy4yNDciLCJjcm4iOiIzZmU4Y2RmMjRmNGJiMTNkNWQ1NmM5NGY2ZjUyMjkwYSIsImhhc2hlZENybiI6IjM1NWMzMTAwZDUzNmVhNGYyZThiYjFmNjQ3YjMwM2U3YWE2MjJmMDU2MGVmYzRiNzk4ODQ2MzQwNmYzYjM1OWIiLCJjYXJkTnVtYmVyIjoiOTM1NTA0OTM3OTMyOSIsImNvcnJlbGF0aW9uSWQiOiJjNjM5ODAxMS1jZTA2LTRlZTgtYmRjNy1mNTMxOWY3NzEyYTciLCJwcmVmZXJlbmNlcyI6eyJpZCI6IjEwNDIiLCJ2YWx1ZSI6ZmFsc2V9fQ==", "messageId": "2652670209994957", "message_id": "2652670209994957", "publishTime": "2021-07-11T10:26:03.697Z", "publish_time": "2021-07-11T10:26:03.697Z"}, "subscription": "projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub"}'
+    req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
+    mock_response = mock.MagicMock()
+    mock_response.status_code = 429
+    mock_get_wallet.side_effect = requests.exceptions.HTTPError(response = mock_response)
+    main.main(req)
+
+    # mock_print.assert_called_with('Timeout error')
+    assert main.main(req) == "500"
+
+
+@mock.patch("main._get_wallet_id_by_crn", autospec=True)
+@mock.patch("main._logging_in_mongodb")
+@mock.patch("main._logging_in_deadletter")
+@mock.patch("builtins.print")
+def test_main_other_request_error(mock_get_wallet, mock_log_mongo, mock_deadletter, mock_print):
+    os.environ["ee_api_url"] = "ee_api_url"
+    os.environ["ee_api_user"] = "ee_api_user"
+    os.environ["ee_api_password"] = "ee_api_password"
+
+    name = "test"
+    data = '{"deliveryAttempt": 1, "message": {"data": "eyJzb3VyY2UiOnsiY29kZSI6MSwibmFtZSI6IkNQT1JUQUwifSwidHJhY2tpbmdJZCI6ImM2Mzk4MDExLWNlMDYtNGVlOC1iZGM3LWY1MzE5Zjc3MTJhNyIsImV2ZW50VHlwZSI6IkNoYW5nZSBQcmVmZXJlbmNlIiwgImV2ZW50U3ViVHlwZSI6Ik5vIGxpcXVvciBPZmZlcnMiLCJwdWJsaXNoZWRBdCI6IjIwMjEtMDUtMTkwMzo1ODoxNy4yNDciLCJjcm4iOiIzZmU4Y2RmMjRmNGJiMTNkNWQ1NmM5NGY2ZjUyMjkwYSIsImhhc2hlZENybiI6IjM1NWMzMTAwZDUzNmVhNGYyZThiYjFmNjQ3YjMwM2U3YWE2MjJmMDU2MGVmYzRiNzk4ODQ2MzQwNmYzYjM1OWIiLCJjYXJkTnVtYmVyIjoiOTM1NTA0OTM3OTMyOSIsImNvcnJlbGF0aW9uSWQiOiJjNjM5ODAxMS1jZTA2LTRlZTgtYmRjNy1mNTMxOWY3NzEyYTciLCJwcmVmZXJlbmNlcyI6eyJpZCI6IjEwNDIiLCJ2YWx1ZSI6ZmFsc2V9fQ==", "messageId": "2652670209994957", "message_id": "2652670209994957", "publishTime": "2021-07-11T10:26:03.697Z", "publish_time": "2021-07-11T10:26:03.697Z"}, "subscription": "projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub"}'
+    req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
+    mock_response = mock.MagicMock()
+    mock_response.status_code = 404
+    mock_get_wallet.side_effect = requests.exceptions.HTTPError(response = mock_response)
+    main.main(req)
+
+    # mock_print.assert_called_with('Timeout error')
+    assert main.main(req) == "102"
+
+
+def test_get_header():
+    authHash = hashlib.sha256("/2.0service_pathPayloadpassword".encode()).hexdigest()
+    transaction_id = 'cde_update_records_ ' + hashlib.md5(str(time.time()).encode()).hexdigest()
+    assert main._get_header("http://localhost", "service_path", "Payload", "authClientId", "password")  == {"X-EES-AUTH-HASH": authHash, "X-EES-AUTH-CLIENT-ID": "authClientId",
+                                                                                                            "X-TRANSACTION-ID": transaction_id,
+                                                                                                            "X-RETRY": "1", "X-EES-OPERATOR": "1", "X-EES-CALLER": "DASHBOARD",
+                                                                                                            "X-EES-CALLER-VERSION": "0",
+                                                                                                            "Content-Type": "application/json"}
+
+
+
+@mock.patch("main._get_header", return_value="Header", autospec=True)
+@mock.patch("main._calling_the_request_consumer_object_function", return_value={'walletId': 123}, autospec=True)
+def test_get_wallet_id_by_crn(mock__get_header, mock__calling_the_request_consumer_object_function):
+    os.environ["ee_get_consumer_service_path"] = "/wallet/{{walletId}}/consumer"
+    os.environ["ee_get_wallet_service_path"] = "/wallet?identity-value={{idenitityValueCRN}}"
+    os.environ["ee_update_consumer_service_path"] = "/wallet/{{walletId}}/consumer/{{consumerId}}"
+    assert main._get_wallet_id_by_crn("url", "authClientId", "password", "crn", "correlationId") == 123
+
+
+
+@mock.patch("main._get_header", return_value="Header", autospec=True)
+@mock.patch("main._calling_the_request_consumer_object_function", return_value={"consumerId": 123}, autospec=True)
+def test_get_consumer_id_by_wallet(mock__get_header, mock__calling_the_request_consumer_object_function):
+    os.environ["ee_get_consumer_service_path"] = "/wallet/{{walletId}}/consumer"
+    os.environ["ee_get_wallet_service_path"] = "/wallet?identity-value={{idenitityValueCRN}}"
+    os.environ["ee_update_consumer_service_path"] = "/wallet/{{walletId}}/consumer/{{consumerId}}"
+    assert main._get_consumer_id_by_wallet("url", "authClientId", "password", "wallet_id", "correlationId") == 123
+
+
+
+@mock.patch("main._get_header", return_value="Header", autospec=True)
+@mock.patch("main._calling_the_request_consumer_object_function", return_value={"walletId": "123"}, autospec=True)
+def test_update_consumer_objects_by_wallet_and_consumer_id(mock_get_header, mock_calling_the_request_consumer_object_function):
+    os.environ["ee_get_consumer_service_path"] = "/wallet/{{walletId}}/consumer"
+    os.environ["ee_get_wallet_service_path"] = "/wallet?identity-value={{idenitityValueCRN}}"
+    os.environ["ee_update_consumer_service_path"] = "/wallet/{{walletId}}/consumer/{{consumerId}}"
+    assert main._update_consumer_objects_by_wallet_and_consumer_id("url", "authClientId", "password", "wallet_id", "consumer_id", "preference_label", "preference_value", "correlationId") == {"walletId": "123"}
+
+
+
+@mock.patch("pymongo.collection.Collection.update_one", return_value = {"a":"b"}, autospec=True)
+def test_logging_in_mongodb(mock_update):
+    # main._logging_in_mongodb("correlationId", "status_code", "status_message", 0) == 
+    os.environ["mongodb_url"] = "mongodb_url"
+    os.environ["mongodb_dbname"] = "mongodb_dbname"
+    os.environ["mongodb_collection"] = "mongodb_collection"
+    main._logging_in_mongodb("correlationId", "status_code", "status_message", 0)
+    mock_update.assert_called_once()
+
+
+
+@mock.patch("google.pubsub_v1.PublisherClient.publish", return_value = {"a":"b"}, autospec=True)
+@mock.patch("concurrent.futures.Future.done", return_value = True)
+def test_logging_in_deadletter(mock_publish, mock_done):
+    os.environ["GCP_PROJECT"] = "GCP_PROJECT"
+    os.environ["error_topic"] = "error_topic"
+    os.environ["FUNCTION_NAME"] = "FUNCTION_NAME"
+
+    main._logging_in_deadletter('event_data', "error_message")
+
+    mock_publish.assert_called_once()
+
+
+'''
+@mock.patch("main._get_wallet_id_by_crn", autospec=True)
+@mock.patch("main._logging_in_mongodb")
+@mock.patch("main._logging_in_deadletter")
+@mock.patch("builtins.print")
+def test_main_timeout(mock_get_wallet, mock_log_mongo, mock_deadletter, mock_print):
+    os.environ["ee_api_url"] = "ee_api_url"
+    os.environ["ee_api_user"] = "ee_api_user"
+    os.environ["ee_api_password"] = "ee_api_password"
+
+    name = "test"
+    data = '{"deliveryAttempt": 1, "message": {"data": "eyJzb3VyY2UiOnsiY29kZSI6MSwibmFtZSI6IkNQT1JUQUwifSwidHJhY2tpbmdJZCI6ImM2Mzk4MDExLWNlMDYtNGVlOC1iZGM3LWY1MzE5Zjc3MTJhNyIsImV2ZW50VHlwZSI6IkNoYW5nZSBQcmVmZXJlbmNlIiwgImV2ZW50U3ViVHlwZSI6Ik5vIGxpcXVvciBPZmZlcnMiLCJwdWJsaXNoZWRBdCI6IjIwMjEtMDUtMTkwMzo1ODoxNy4yNDciLCJjcm4iOiIzZmU4Y2RmMjRmNGJiMTNkNWQ1NmM5NGY2ZjUyMjkwYSIsImhhc2hlZENybiI6IjM1NWMzMTAwZDUzNmVhNGYyZThiYjFmNjQ3YjMwM2U3YWE2MjJmMDU2MGVmYzRiNzk4ODQ2MzQwNmYzYjM1OWIiLCJjYXJkTnVtYmVyIjoiOTM1NTA0OTM3OTMyOSIsImNvcnJlbGF0aW9uSWQiOiJjNjM5ODAxMS1jZTA2LTRlZTgtYmRjNy1mNTMxOWY3NzEyYTciLCJwcmVmZXJlbmNlcyI6eyJpZCI6IjEwNDIiLCJ2YWx1ZSI6ZmFsc2V9fQ==", "messageId": "2652670209994957", "message_id": "2652670209994957", "publishTime": "2021-07-11T10:26:03.697Z", "publish_time": "2021-07-11T10:26:03.697Z"}, "subscription": "projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub"}'
+    req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
+    mock_get_wallet.side_effect = requests.Timeout
+    main.main(req)
+
+    # mock_print.assert_called_with('Timeout error')
+    assert main.main(req) == "500"
+'''

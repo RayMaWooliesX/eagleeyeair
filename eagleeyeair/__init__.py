6a3ee878e384ce37677675c71070d53b1d0db87b
cg1101 <cg1101@gmail.com>
2021-08-30 14:27:47 +1000

    fix: use None for default value of data for patch/put

--- eagleeyeair/__init__.py
+++ eagleeyeair/__init__.py
@@ -4,12 +4,12 @@ from .pos import EagleEyePos
 from .resources import EagleEyeResources
 from .wallet import EagleEyeWallet
 
-client_id = os.environ.get('EES_AUTH_CLIENT_ID', '')
-secret = os.environ.get('EES_AUTH_CLIENT_SECRET', '')
-prefix = os.environ.get('EES_API_PREFIX', '')
-pos_host = os.environ.get('EES_POS_API_HOST', '')
-resources_host = os.environ.get('EES_RESOURCES_API_HOST', '')
-wallet_host = os.environ.get('EES_WALLET_API_HOST', '')
+client_id = os.environ.get("EES_AUTH_CLIENT_ID", "")
+secret = os.environ.get("EES_AUTH_CLIENT_SECRET", "")
+prefix = os.environ.get("EES_API_PREFIX", "")
+pos_host = os.environ.get("EES_POS_API_HOST", "")
+resources_host = os.environ.get("EES_RESOURCES_API_HOST", "")
+wallet_host = os.environ.get("EES_WALLET_API_HOST", "")
 
 pos = EagleEyePos(pos_host, prefix, client_id, secret)
 resources = EagleEyeResources(resources_host, prefix, client_id, secret)
--- eagleeyeair/__main__.py
+++ eagleeyeair/__main__.py
@@ -1,6 +1,6 @@
-
 def main():
-    print('Thank you for using eagleeyeair.')
+    print("Thank you for using eagleeyeair.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

a7943fc354754f3bf77215ed49d92a704f94c1a4
RayMaWooliesX <75294321+RayMaWooliesX@users.noreply.github.com>
2021-09-28 17:00:34 +1000

    Develop (#35)
    * remove uneccessary env vars (#32)
    
    * EDN-577-Membercard API - Cancel

--- src_cards/main.py
+++ src_cards/main.py
@@ -9,12 +9,12 @@ import os
 import base64
 import hashlib
 import time
-
 from datetime import datetime, timezone
-import pytz
-import json
 import logging
 import traceback
+
+import pytz
+import json
 import requests
 import pymongo
 from pymongo import MongoClient
@@ -22,14 +22,7 @@ from google.cloud import pubsub_v1
 from google.cloud import error_reporting
 from flask import request
 
-import eagleeyeair
-
-client_id = os.environ.get("EES_AUTH_CLIENT_ID", "")
-secret = os.environ.get("EES_AUTH_CLIENT_SECRET", "")
-prefix = os.environ.get("EES_API_PREFIX", "")
-pos_host = os.environ.get("EES_POS_API_HOST", "")
-resources_host = os.environ.get("EES_RESOURCES_API_HOST", "")
-wallet_host = os.environ.get("EES_WALLET_API_HOST", "")
+import eagleeyeair as ee
 
 def main_cards(request):
     """ Responds to an HTTP request using data from the request body parsed
@@ -42,54 +35,45 @@ def main_cards(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """ 
-    error_client = error_reporting.Client()
+    event_sub_types = {"cancel", "replace", "reregister", "deregister"}
+    event_data, delivery_attempt = _parse_request(request)
 
-    try:
-        event_data, delivery_attempt = _parse_request(request)
+    if event_data["eventType"] != "cards":
+        raise RuntimeError("Unexpected event type: " + event_data["eventType"])
 
-        wallet_id = eagleeyeair.wallet.get_wallet_by_identity_value(event_data["eventDetails"]["profile"]["crn"])["walletId"]
-        ###temp code
-        wallet_api = ee_api(wallet_host, prefix, client_id, secret)
-        ###
-        identities = wallet_api.get_wallet_identities_by_wallet_id_with_no_query(wallet_id)["results"]
-        identity_id = ""
+    if event_data["eventSubType"] not in event_sub_types:
+        raise RuntimeError("Unexpected event sub type: " + event_data["eventSubType"])
 
-        for identity in identities:
-            if identity["value"] == event_data["eventDetails"]["profile"]["account"]["cardNumber"]:
-                identity_id = identity["identityId"]
-        eagleeyeair.wallet.update_wallet_identity_status_suspended(wallet_id= wallet_id, identity_id= identity_id)
+    wallet_id = ee.wallet.get_wallet_by_identity_value(event_data["eventDetails"]["profile"]["crn"])["walletId"]
+    identities = ee.wallet.get_wallet_identities_by_wallet_id(wallet_id, type='LCN')["results"]
 
-        if event_data["eventSubType"] == "replace":
-            data = _prepare_lcn_payload(event_data["eventDetails"]["profile"]["account"]["newCardNumber"])
-            eagleeyeair.wallet.create_wallet_identity(wallet_id,data)
-        response_code = '200'
-        return '200'
+    identity_id = ""
+    for identity in identities:
+        if identity["value"] == event_data["eventDetails"]["profile"]["account"]["cardNumber"]:
+            identity_id = identity["identityId"]
+    if identity_id == "":
+        raise RuntimeError("LCN number: " 
+                        + event_data["eventDetails"]["profile"]["account"]["cardNumber"]
+                        + " can't be found in EE.")
+    
+    if event_data["eventSubType"] == "replace":
+        data = _prepare_lcn_payload(event_data["eventDetails"]["profile"]["account"]["cards"]["newCardNumber"])
+        ee.wallet.create_wallet_identity(wallet_id,data)
 
-    except requests.exceptions.RequestException as e:
-        if e.response.status_code == 429:
-            response_code = '500'
-            logging.error(RuntimeError("Too many requests error"))
-            logging.info(traceback.format_exc())
-            error_client.report_exception()
-            logging.info("Too many requests error logging completed.")
+    if event_data["eventSubType"] in ("replace", "cancel"):
+        if event_data["eventSubType"]  == "replace":
+            cancel_reason = event_data["eventDetails"]["profile"]["account"]["cards"]["replacementReason"].upper()
+        else: 
+            cancel_reason = event_data["eventDetails"]["profile"]["account"]["cards"]["cancellationReasonDescription"].upper()
+        
+        if cancel_reason == "LOST":
+            ee.wallet.update_wallet_identity_status_lost(wallet_id= wallet_id, identity_id= identity_id)
+        elif cancel_reason == "STOLEN":
+            ee.wallet.update_wallet_identity_status_stolen(wallet_id= wallet_id, identity_id= identity_id)
         else:
-            response_code = '102'
-            logging.error(RuntimeError("Http error: "))
-            logging.info(traceback.format_exc())
-            error_client.report_exception()
-            logging.error(RuntimeError)
-            _logging_in_deadletter(json.dumps(event_data), e.response.reason)
-            logging.info("Http error logging completed.")
-    except Exception as e:
-        response_code = '204'
-        logging.error(traceback.format_exc())
-        logging.error(RuntimeError('Data error:'))
-        error_msg = ','.join(e.args) + "," + e.__doc__
-        logging.error(RuntimeError(error_msg))
-        error_client.report_exception()
-        _logging_in_deadletter(json.dumps(event_data), error_msg)
-    finally:
-        return response_code
+            ee.wallet.update_wallet_identity_status_suspended(wallet_id= wallet_id, identity_id= identity_id)
+
+    return '200'
 
 def _prepare_lcn_payload(lcn_num):
     return {
@@ -112,7 +96,7 @@ def _parse_request(request):
     logging.info('Completed preparing data for EE request.')
     return event_data, delivery_attempt
 
-def _logging_in_deadletter(event_data, error_message):
+
 
     try:
         logging.info("---Logging original message to dead letter topic.")
@@ -131,13 +115,3 @@ def _logging_in_deadletter(event_data, error_message):
         logging.error(RuntimeError("!!! There was an error while sending error message to dead letter topic. " ))
         logging.info(traceback.format_exc())
         pass
-
-
-class ee_api(eagleeyeair.EagleEyeWallet):
-    def get_wallet_identities_by_wallet_id_with_no_query(self,
-        wallet_id,
-        type=[]
-    ):
-        return self.get(
-            f"/wallet/{wallet_id}/identities"
-        )

3739a2cb318cca0fd634f338fffc236c8ed0d1d6
RayMaWooliesX <qma@woolworths.com.au>
2021-09-28 16:36:35 +1000

    EDN-577-Membercard API - Cancel

--- src_cards/test_main.py
+++ src_cards/test_main.py
@@ -21,79 +21,87 @@ from pymongo.collection import Collection
 from flask import Flask
 import requests
 import logging
+from datetime import datetime
 
-logger = logging.getLogger('eagle_eye_api')
-logger.setLevel(logging.DEBUG)
+import eagleeyeair
 
-
-
+try:
+  eagleeyeair.wallet.update_wallet_identity_status_active("114413159", "87930461")
+  eagleeyeair.wallet.update_wallet_identity_status_active("114305351", "87863093")
+except Exception as e:
+  print(e)
 
 @mock.patch("main._parse_request", return_value = ({
-            "eventType": "cards",
-            "eventSubType": "cancel",
-            "operation": "delete",
-            "eventDetails": {
-                "source": {
-                "code": 1,
-                "name": "CPORTAL"
-                },
-                "trackingId": "a9cee8c2-72a5-44b7-bca6-e15d4f91dc8d",
-                "correlationId": "4b5b39c2-d012-431d-8b40-d0e77ffa9f26",
-                "publishedAt": "2021-09-07T05:08:34UTC",
-                "profile": {
-                "crn": "3fe8cdf24f4bb13d5d56c94f6f52290a",
-                "crnHash": "559256253_hash",
-                "account": {
-                    "accountType": {
-                    "code": 1002,
-                    "name": "EDR"
-                    },
-                    "cardNumber": "9e98c906459a7e0b5272913d3a58589c",
-                    "cards": {}
-                }
-                }
-            }
-            },1))
-def test_main_cards(mock_parse_request):
+    "eventType": "cards",
+    "eventSubType": "cancel",
+    "operation": "update",
+    "eventDetails": {
+      "source": {
+        "code": 1,
+        "name": "CPORTAL"
+      },
+      "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
+      "publishedAt": "2018-11-11T11:01:59+11:11",
+      "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
+      "profile": {
+        "crn": "622589b75ea8cb24c3d3b505379b26fe",
+        "crnHash": "7a30398d3e11bfecb7c9e7b014adfgdfg6463c768cc35b942d6ec44af66f185",
+        "account": {
+          "accountType": {
+            "code": 1002,
+            "name": "EDR"
+          },
+          "cardNumber": "d309d83e9f408633597ec19da315880c",
+          "cards": {
+            "cancelledCardNumber": "d309d83e9f408633597ec19da315880c",
+            "cancellationReasonCode": "123",
+            "cancellationReasonDescription": "LOST",
+            "cancellationComment": "Not getting many offers",
+            "cancellationRequestDatetime": "2018-11-11T11:01:59+11:11"
+          }
+        }
+      }
+    }
+  },1))
+def test_main_cards_cancel(mock_parse_request):
     data = {'deliveryAttempt': 1, 'message': {'data': 'eyJldmVudFR5cGUiOiAicHJlZmVyZW5jZXMiLCAiZXZlbnRTdWJUeXBlIjogInJlZGVtcHRpb24iLCAib3BlcmF0aW9uIjogInVwZGF0ZSIsICJldmVudERldGFpbHMiOiB7InNvdXJjZSI6IHsiY29kZSI6IDEsICJuYW1lIjogIkNQT1JUQUwifSwgInRyYWNraW5nSWQiOiAiMWI2NzFhNjQtNDBkNS00OTFlLTk5YjAtZGEwMWZmMWYzMzQxIiwgInB1Ymxpc2hlZEF0IjogIjIwMTgtMTEtMTFUMTE6MDE6NTkrMTE6MTEiLCAiY29ycmVsYXRpb25JZCI6ICJhOGVlNmE5MC1jY2JjLTQ2NzgtODIzMC1hNGE2NWZiZjcwMDQiLCAicHJvZmlsZSI6IHsiY3JuIjogIjMzMDAwMDAwMDAwMzQ1MzU3NDgiLCAiY3JuSGFzaCI6ICI3YTMwMzk4ZDNlMTFiZmVjYjdjOWU3YjAxNGFkZmdkZmc2NDYzYzc2OGNjMzViOTQyZDZlYzQ0YWY2NmYxODUiLCAiYWNjb3VudCI6IHsiYWNjb3VudFR5cGUiOiB7ImNvZGUiOiAxMDAyLCAibmFtZSI6ICJFRFIifSwgImNhcmROdW1iZXIiOiAiOTM1NTA0OTM3OTMyOSIsICJwcmVmZXJlbmNlcyI6IFt7ImlkIjogMTAyNCwgIm5hbWUiOiAiVW5zdWJzY3JpYmUgQWxsIiwgInZhbHVlIjogIlFGRiJ9XX19fX0=', 'messageId': '2754332662377047', 'message_id': '2754332662377047', 'publishTime': '2021-08-01T13:48:51.299Z', 'publish_time': '2021-08-01T13:48:51.299Z'}, 'subscription': 'projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub'}
     req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
 
-    
-    assert main.main_cards(req) == "200"
-
+    assert main.main_cards(data) == "200"
 
 
 @mock.patch("main._parse_request", return_value = ({
-  "eventType": "cards",
-  "eventSubType": "replace",
-  "operation": "delete",
-  "eventDetails": {
-    "source": {
-      "code": 1,
-      "name": "CPORTAL"
-    },
-    "trackingId": "a9cee8c2-72a5-44b7-bca6-e15d4f91dc8d",
-    "correlationId": "4b5b39c2-d012-431d-8b40-d0e77ffa9f26",
-    "publishedAt": "2021-09-07T05:08:34UTC",
-    "profile": {
-      "crn": "9942cc5d2bfee0e540d5801a483aa882",
-      "crnHash": "3d05e22d0c398e642a3b5ffe815461306b99c901ee57682a52b0827c3c270e08",
-      "account": {
-        "accountType": {
-          "code": 1002,
-          "name": "EDR"
-        },
-        "cardNumber": "a90ce8514780380e5cd752ae6151d620",
-        "newCardNumber": "a90ce8514780380e5cd752ae6151d620new",
-        "cards": {}
+    "eventType": "cards",
+    "eventSubType": "replace",
+    "operation": "update",
+    "eventDetails": {
+      "source": {
+        "code": 1,
+        "name": "CPORTAL"
+      },
+      "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
+      "publishedAt": "2018-11-11T11:01:59+11:11",
+      "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
+      "profile": {
+        "crn": "9942cc5d2bfee0e540d5801a483aa882",
+        "crnHash": "7a30398d3e11bfecb7c9e7b014adfgdfg6463c768cc35b942d6ec44af66f185",
+        "account": {
+          "accountType": {
+            "code": 1002,
+            "name": "EDR"
+          },
+          "cardNumber": "a90ce8514780380e5cd752ae6151d620",
+          "cards": {
+            "oldCardNumber": "a90ce8514780380e5cd752ae6151d620",
+            "newCardNumber": "a90ce8514780380e5cd752ae6151d620new"+datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)"),
+            "replacementReason": "Lost"
+          }
+        }
       }
     }
-  }
-},1))
+  },1))
 def test_main_cards_replace(mock_parse_request):
     data = {'deliveryAttempt': 1, 'message': {'data': 'eyJldmVudFR5cGUiOiAicHJlZmVyZW5jZXMiLCAiZXZlbnRTdWJUeXBlIjogInJlZGVtcHRpb24iLCAib3BlcmF0aW9uIjogInVwZGF0ZSIsICJldmVudERldGFpbHMiOiB7InNvdXJjZSI6IHsiY29kZSI6IDEsICJuYW1lIjogIkNQT1JUQUwifSwgInRyYWNraW5nSWQiOiAiMWI2NzFhNjQtNDBkNS00OTFlLTk5YjAtZGEwMWZmMWYzMzQxIiwgInB1Ymxpc2hlZEF0IjogIjIwMTgtMTEtMTFUMTE6MDE6NTkrMTE6MTEiLCAiY29ycmVsYXRpb25JZCI6ICJhOGVlNmE5MC1jY2JjLTQ2NzgtODIzMC1hNGE2NWZiZjcwMDQiLCAicHJvZmlsZSI6IHsiY3JuIjogIjMzMDAwMDAwMDAwMzQ1MzU3NDgiLCAiY3JuSGFzaCI6ICI3YTMwMzk4ZDNlMTFiZmVjYjdjOWU3YjAxNGFkZmdkZmc2NDYzYzc2OGNjMzViOTQyZDZlYzQ0YWY2NmYxODUiLCAiYWNjb3VudCI6IHsiYWNjb3VudFR5cGUiOiB7ImNvZGUiOiAxMDAyLCAibmFtZSI6ICJFRFIifSwgImNhcmROdW1iZXIiOiAiOTM1NTA0OTM3OTMyOSIsICJwcmVmZXJlbmNlcyI6IFt7ImlkIjogMTAyNCwgIm5hbWUiOiAiVW5zdWJzY3JpYmUgQWxsIiwgInZhbHVlIjogIlFGRiJ9XX19fX0=', 'messageId': '2754332662377047', 'message_id': '2754332662377047', 'publishTime': '2021-08-01T13:48:51.299Z', 'publish_time': '2021-08-01T13:48:51.299Z'}, 'subscription': 'projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub'}
     req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
 
-    
-    assert main.main_cards(req) == "200"
-
+    assert main.main_cards(data) == "200"

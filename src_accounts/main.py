d56eb7c8e70b0c756e82319b1926209f0de676a2
RayMaWooliesX <75294321+RayMaWooliesX@users.noreply.github.com>
2021-11-24 11:26:08 +1100

    Develop (#45)
    EDN-72-added the segmentation logic for card api

--- src_accounts/main.py
+++ src_accounts/main.py
@@ -1,33 +1,24 @@
-'''
-This Cloud function is responsible for:
-- Parsing data triggered by source pubsub
-- Preparing data for the EE API calls.
-- Calling EE APIs to update preference
-'''
-
 import os
 import base64
-import time
 from datetime import datetime, timezone
 import logging
-import traceback
+
 
 import json
-from eagleeyeair import wallet
 import jsonschema
-from google.cloud import pubsub_v1
 
 import eagleeyeair as ee
 
+
 def main_accounts(request):
-    """ Responds to an HTTP request using data from the request body parsed
-    according to the "content-type" header.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    More detail information can be found in the LLD below:
-    
-    """   
+    """Responds to an HTTP request using data from the request body parsed
+        according to the "content-type" header.
+        Args:
+            request (flask.Request): The request object.
+            <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+        More detail information can be found in the LLD below:
+    https://woolworthsdigital.atlassian.net/wiki/spaces/DGDMS/pages/25556287773/Detailed+Design+for+Loyalty+API+Integration+of+account+management
+    """
     event_data, delivery_attempt = _parse_request(request)
     event_sub_types = ["register"]
 
@@ -38,72 +29,96 @@ def main_accounts(request):
         raise RuntimeError("Unexpected event sub type: " + event_data["eventSubType"])
 
     # register account
-    if event_data["eventSubType"] == 'register':
+    if event_data["eventSubType"] == "register":
         wallet_payload = _prepare_wallet_payload(event_data)
         wallet = ee.wallet.create_wallet_and_wallet_identities(wallet_payload)
 
-        ee.wallet.create_wallet_consumer(wallet["walletId"], 
-                                        {
-                                        "friendlyName": "DefaultConsumer",
-                                        "type": "DEFAULT",
-                                        }
-                                        )
+        consumer = ee.wallet.create_wallet_consumer(
+            wallet["walletId"],
+            {
+                "friendlyName": "DefaultConsumer",
+                "type": "DEFAULT",
+            },
+        )
 
-        ee.wallet.create_wallet_scheme_account(wallet["walletId"], 
-                                            os.environ["ee_memberSchemeId"], 
-                                            {"status": "ACTIVE", "state": "LOADED"}
-                                            )   
-                      
-    return '200'
+        ee.wallet.create_wallet_scheme_account(
+            wallet["walletId"],
+            os.environ["ee_memberSchemeId"],
+            {"status": "ACTIVE", "state": "LOADED"},
+        )
+
+        ee.wallet.update_wallet_consumer(
+            wallet["walletId"],
+            consumer["consumerId"],
+            {
+                "friendlyName": "Sample Consumer",
+                "data": {
+                    "segmentation": [
+                        {
+                            "name": "memberOfferTarget",
+                            "segments": [
+                                {
+                                    "labels": ["Member Offer Target"],
+                                    "data": {"0101": "EDR Registered card"},
+                                }
+                            ],
+                        }
+                    ]
+                },
+            },
+        )
+
+    return "200"
+
 
 def _prepare_wallet_payload(event_data):
     return {
-            "type": "MEMBER",
-            "status": "ACTIVE",
-            "state": "EARNBURN",
-            "identities": [
-                {
+        "type": "MEMBER",
+        "status": "ACTIVE",
+        "state": "EARNBURN",
+        "identities": [
+            {
                 "type": "LCN",
                 "friendlyName": "Loyalty Card Number",
                 "value": event_data["eventDetails"]["profile"]["account"]["cardNumber"],
                 "state": "REGISTERED",
-                "status": "ACTIVE"
-                },
-                {
+                "status": "ACTIVE",
+            },
+            {
                 "type": "CRN",
                 "friendlyName": "Customer Reference Number",
                 "value": event_data["eventDetails"]["profile"]["crn"],
                 "state": "REGISTERED",
-                "status": "ACTIVE"
-                },
-                {
+                "status": "ACTIVE",
+            },
+            {
                 "type": "HASH_CRN",
                 "friendlyName": "Hashed Customer Reference Number",
                 "value": event_data["eventDetails"]["profile"]["crnHash"],
                 "state": "REGISTERED",
-                "status": "ACTIVE"
-                }]
-            }
+                "status": "ACTIVE",
+            },
+        ],
+    }
+
 
 def _parse_request(request):
-    '''parse the request and return the request data in jason format
-    '''
-    logging.info('Preparing data for EE request.')
+    """parse the request and return the request data in jason format"""
+    logging.info("Preparing data for EE request.")
     envelope = request.get_json()
-    message = envelope['message']
-    delivery_attempt = envelope['deliveryAttempt']
-    event_data_str = base64.b64decode(message['data'])
+    message = envelope["message"]
+    delivery_attempt = envelope["deliveryAttempt"]
+    event_data_str = base64.b64decode(message["data"])
     event_data = json.loads(event_data_str)
     validate_payload_format(event_data)
 
-    logging.info('Completed preparing data for EE request.')
+    logging.info("Completed preparing data for EE request.")
     return event_data, delivery_attempt
 
 
 def validate_payload_format(event_data):
     "this function validate the message payload format againt the schema"
-    with open('account-register-schema.json', 'r') as file:
+    with open("account-register-schema.json", "r") as file:
         schema = json.load(file)
-    
-    jsonschema.validate(instance=event_data, schema=schema)
 
+    jsonschema.validate(instance=event_data, schema=schema)
--- src_cards/test_main.py
+++ src_cards/test_main.py
@@ -1,5 +1,5 @@
-
 import os
+
 os.environ["EES_AUTH_CLIENT_ID"] = "u2v2hsh07xvju1eex8eh"
 os.environ["EES_AUTH_CLIENT_SECRET"] = "tplzi3t8v6okghbrrhts0644dep5ns"
 os.environ["EES_API_PREFIX"] = "/2.0"
@@ -25,157 +25,251 @@ from datetime import datetime
 import eagleeyeair
 
 try:
-  # set up data for replace/cancel
-  eagleeyeair.wallet.update_wallet_identity_status_active("114413159", "87930461")
-  eagleeyeair.wallet.update_wallet_identity_status_active("114305351", "87863093")
+    # set up data for cancel
+    eagleeyeair.wallet.update_wallet_identity_status_active("114413159", "87930461")
+    eagleeyeair.wallet.update_wallet_consumer(
+        "114413159",
+        "18088672",
+        {
+            "friendlyName": "Sample Consumer",
+            "data": {
+                "segmentation": [
+                    {
+                        "name": "localEnvironmentSegments",
+                        "segments": [
+                            {
+                                "labels": ["Local Environment Segments"],
+                                "data": {"0090": "Layby Modify Txn"},
+                            }
+                        ],
+                    },
+                    {
+                        "name": "localTargetSegments",
+                        "segments": [
+                            {
+                                "labels": ["Local Target Segments"],
+                                "data": {"0005": "Staff Discount Card"},
+                            }
+                        ],
+                    },
+                    {
+                        "name": "memberOfferTarget",
+                        "segments": [
+                            {
+                                "labels": ["Member Offer Target"],
+                                "data": {
+                                    "0062": "BWS Staff",
+                                    "0063": "Big W Staff",
+                                    "0101": "EDR Registered card",
+                                },
+                            }
+                        ],
+                    },
+                    {
+                        "name": "memberOfferExclusions",
+                        "segments": [
+                            {
+                                "labels": ["Member Offer Exclusions"],
+                                "data": {"0047": "No Liquor Offers"},
+                            }
+                        ],
+                    },
+                    {
+                        "name": "memberPreferences",
+                        "segments": [
+                            {
+                                "labels": ["Member Preferences"],
+                                "data": {
+                                    "0033": "eReceipt - Supermarkets & Metro",
+                                    "0035": "eReceipt - BWS",
+                                    "0037": "eReceipt - Big W",
+                                    "0104": "SFL QFF",
+                                },
+                            }
+                        ],
+                    },
+                ]
+            },
+        },
+    )
+    # set up data for replace
+    eagleeyeair.wallet.update_wallet_identity_status_active("114305351", "87863093")
 
-  # set the wallet and identity status/state for reregister
-  eagleeyeair.wallet.update_wallet_state(wallet_id= "114482846", data = {"state": "CLOSED"})
-  eagleeyeair.wallet.suspend_wallet("114482846")
+    # set the wallet and identity status/state for reregister
+    eagleeyeair.wallet.update_wallet_state(
+        wallet_id="114801331", data={"state": "CLOSED"}
+    )
+    eagleeyeair.wallet.suspend_wallet("114801331")
 
 except Exception as e:
-  print(e)
+    print(e)
 
-@mock.patch("main._parse_request", return_value = ({
-    "eventType": "cards",
-    "eventSubType": "cancel",
-    "operation": "update",
-    "eventDetails": {
-      "source": {
-        "code": 1,
-        "name": "CPORTAL"
-      },
-      "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
-      "publishedAt": "2018-11-11T11:01:59+11:11",
-      "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
-      "profile": {
-        "crn": "622589b75ea8cb24c3d3b505379b26fe",
-        "crnHash": "7a30398d3e11bfecb7c9e7b014adfgdfg6463c768cc35b942d6ec44af66f185",
-        "account": {
-          "accountType": {
-            "code": 1002,
-            "name": "EDR"
-          },
-          "cardNumber": "d309d83e9f408633597ec19da315880c",
-          "cards": {
-            "cancelledCardNumber": "d309d83e9f408633597ec19da315880c",
-            "cancellationReasonCode": "123",
-            "cancellationReasonDescription": "LOST",
-            "cancellationComment": "Not getting many offers",
-            "cancellationRequestDatetime": "2018-11-11T11:01:59+11:11"
-          }
-        }
-      }
-    }
-  },1))
+
+@mock.patch(
+    "main._parse_request",
+    return_value=(
+        {
+            "eventType": "cards",
+            "eventSubType": "cancel",
+            "operation": "update",
+            "eventDetails": {
+                "source": {"code": 1, "name": "CPORTAL"},
+                "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
+                "publishedAt": "2018-11-11T11:01:59+11:11",
+                "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
+                "profile": {
+                    "crn": "622589b75ea8cb24c3d3b505379b26fe",
+                    "crnHash": "94b43035b41b54fea20c7e8bea4e5c43cc3a6fc9137a41c1d330f1e481c5c7d5",
+                    "account": {
+                        "accountType": {"code": 1002, "name": "EDR"},
+                        "cardNumber": "d309d83e9f408633597ec19da315880c",
+                        "cards": {
+                            "cancelledCardNumber": "d309d83e9f408633597ec19da315880c",
+                            "cancellationReasonCode": "123",
+                            "cancellationReasonDescription": "LOST",
+                            "cancellationComment": "Not getting many offers",
+                            "cancellationRequestDatetime": "2018-11-11T11:01:59+11:11",
+                        },
+                    },
+                },
+            },
+        },
+        1,
+    ),
+)
 def test_main_cards_cancel(mock_parse_request):
-    data = {'deliveryAttempt': 1, 'message': {'data': 'eyJldmVudFR5cGUiOiAicHJlZmVyZW5jZXMiLCAiZXZlbnRTdWJUeXBlIjogInJlZGVtcHRpb24iLCAib3BlcmF0aW9uIjogInVwZGF0ZSIsICJldmVudERldGFpbHMiOiB7InNvdXJjZSI6IHsiY29kZSI6IDEsICJuYW1lIjogIkNQT1JUQUwifSwgInRyYWNraW5nSWQiOiAiMWI2NzFhNjQtNDBkNS00OTFlLTk5YjAtZGEwMWZmMWYzMzQxIiwgInB1Ymxpc2hlZEF0IjogIjIwMTgtMTEtMTFUMTE6MDE6NTkrMTE6MTEiLCAiY29ycmVsYXRpb25JZCI6ICJhOGVlNmE5MC1jY2JjLTQ2NzgtODIzMC1hNGE2NWZiZjcwMDQiLCAicHJvZmlsZSI6IHsiY3JuIjogIjMzMDAwMDAwMDAwMzQ1MzU3NDgiLCAiY3JuSGFzaCI6ICI3YTMwMzk4ZDNlMTFiZmVjYjdjOWU3YjAxNGFkZmdkZmc2NDYzYzc2OGNjMzViOTQyZDZlYzQ0YWY2NmYxODUiLCAiYWNjb3VudCI6IHsiYWNjb3VudFR5cGUiOiB7ImNvZGUiOiAxMDAyLCAibmFtZSI6ICJFRFIifSwgImNhcmROdW1iZXIiOiAiOTM1NTA0OTM3OTMyOSIsICJwcmVmZXJlbmNlcyI6IFt7ImlkIjogMTAyNCwgIm5hbWUiOiAiVW5zdWJzY3JpYmUgQWxsIiwgInZhbHVlIjogIlFGRiJ9XX19fX0=', 'messageId': '2754332662377047', 'message_id': '2754332662377047', 'publishTime': '2021-08-01T13:48:51.299Z', 'publish_time': '2021-08-01T13:48:51.299Z'}, 'subscription': 'projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub'}
+    data = {
+        "deliveryAttempt": 1,
+        "message": {},
+        "subscription": "projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub",
+    }
     req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
 
     assert main.main_cards(data) == "200"
 
 
-@mock.patch("main._parse_request", return_value = ({
-    "eventType": "cards",
-    "eventSubType": "replace",
-    "operation": "update",
-    "eventDetails": {
-      "source": {
-        "code": 1,
-        "name": "CPORTAL"
-      },
-      "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
-      "publishedAt": "2018-11-11T11:01:59+11:11",
-      "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
-      "profile": {
-        "crn": "9942cc5d2bfee0e540d5801a483aa882",
-        "crnHash": "7a30398d3e11bfecb7c9e7b014adfgdfg6463c768cc35b942d6ec44af66f185",
-        "account": {
-          "accountType": {
-            "code": 1002,
-            "name": "EDR"
-          },
-          "cardNumber": "a90ce8514780380e5cd752ae6151d620",
-          "cards": {
-            "oldCardNumber": "a90ce8514780380e5cd752ae6151d620",
-            "newCardNumber": "a90ce8514780380e5cd752ae6151d620new"+datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)"),
-            "replacementReason": "Lost"
-          }
-        }
-      }
-    }
-  },1))
+@mock.patch(
+    "main._parse_request",
+    return_value=(
+        {
+            "eventType": "cards",
+            "eventSubType": "replace",
+            "operation": "update",
+            "eventDetails": {
+                "source": {"code": 1, "name": "CPORTAL"},
+                "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
+                "publishedAt": "2018-11-11T11:01:59+11:11",
+                "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
+                "profile": {
+                    "crn": "9942cc5d2bfee0e540d5801a483aa882",
+                    "crnHash": "3d05e22d0c398e642a3b5ffe815461306b99c901ee57682a52b0827c3c270e08",
+                    "account": {
+                        "accountType": {"code": 1002, "name": "EDR"},
+                        "cardNumber": "a90ce8514780380e5cd752ae6151d620",
+                        "cards": {
+                            "oldCardNumber": "a90ce8514780380e5cd752ae6151d620",
+                            "newCardNumber": "a90ce8514780380e5cd752ae6151d620new"
+                            + datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)"),
+                            "replacementReason": "Lost",
+                        },
+                    },
+                },
+            },
+        },
+        1,
+    ),
+)
 def test_main_cards_replace(mock_parse_request):
-    data = {'deliveryAttempt': 1, 'message': {'data': 'eyJldmVudFR5cGUiOiAicHJlZmVyZW5jZXMiLCAiZXZlbnRTdWJUeXBlIjogInJlZGVtcHRpb24iLCAib3BlcmF0aW9uIjogInVwZGF0ZSIsICJldmVudERldGFpbHMiOiB7InNvdXJjZSI6IHsiY29kZSI6IDEsICJuYW1lIjogIkNQT1JUQUwifSwgInRyYWNraW5nSWQiOiAiMWI2NzFhNjQtNDBkNS00OTFlLTk5YjAtZGEwMWZmMWYzMzQxIiwgInB1Ymxpc2hlZEF0IjogIjIwMTgtMTEtMTFUMTE6MDE6NTkrMTE6MTEiLCAiY29ycmVsYXRpb25JZCI6ICJhOGVlNmE5MC1jY2JjLTQ2NzgtODIzMC1hNGE2NWZiZjcwMDQiLCAicHJvZmlsZSI6IHsiY3JuIjogIjMzMDAwMDAwMDAwMzQ1MzU3NDgiLCAiY3JuSGFzaCI6ICI3YTMwMzk4ZDNlMTFiZmVjYjdjOWU3YjAxNGFkZmdkZmc2NDYzYzc2OGNjMzViOTQyZDZlYzQ0YWY2NmYxODUiLCAiYWNjb3VudCI6IHsiYWNjb3VudFR5cGUiOiB7ImNvZGUiOiAxMDAyLCAibmFtZSI6ICJFRFIifSwgImNhcmROdW1iZXIiOiAiOTM1NTA0OTM3OTMyOSIsICJwcmVmZXJlbmNlcyI6IFt7ImlkIjogMTAyNCwgIm5hbWUiOiAiVW5zdWJzY3JpYmUgQWxsIiwgInZhbHVlIjogIlFGRiJ9XX19fX0=', 'messageId': '2754332662377047', 'message_id': '2754332662377047', 'publishTime': '2021-08-01T13:48:51.299Z', 'publish_time': '2021-08-01T13:48:51.299Z'}, 'subscription': 'projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub'}
+    data = {
+        "deliveryAttempt": 1,
+        "message": {
+            "data": "eyJldmVudFR5cGUiOiAicHJlZmVyZW5jZXMiLCAiZXZlbnRTdWJUeXBlIjogInJlZGVtcHRpb24iLCAib3BlcmF0aW9uIjogInVwZGF0ZSIsICJldmVudERldGFpbHMiOiB7InNvdXJjZSI6IHsiY29kZSI6IDEsICJuYW1lIjogIkNQT1JUQUwifSwgInRyYWNraW5nSWQiOiAiMWI2NzFhNjQtNDBkNS00OTFlLTk5YjAtZGEwMWZmMWYzMzQxIiwgInB1Ymxpc2hlZEF0IjogIjIwMTgtMTEtMTFUMTE6MDE6NTkrMTE6MTEiLCAiY29ycmVsYXRpb25JZCI6ICJhOGVlNmE5MC1jY2JjLTQ2NzgtODIzMC1hNGE2NWZiZjcwMDQiLCAicHJvZmlsZSI6IHsiY3JuIjogIjMzMDAwMDAwMDAwMzQ1MzU3NDgiLCAiY3JuSGFzaCI6ICI3YTMwMzk4ZDNlMTFiZmVjYjdjOWU3YjAxNGFkZmdkZmc2NDYzYzc2OGNjMzViOTQyZDZlYzQ0YWY2NmYxODUiLCAiYWNjb3VudCI6IHsiYWNjb3VudFR5cGUiOiB7ImNvZGUiOiAxMDAyLCAibmFtZSI6ICJFRFIifSwgImNhcmROdW1iZXIiOiAiOTM1NTA0OTM3OTMyOSIsICJwcmVmZXJlbmNlcyI6IFt7ImlkIjogMTAyNCwgIm5hbWUiOiAiVW5zdWJzY3JpYmUgQWxsIiwgInZhbHVlIjogIlFGRiJ9XX19fX0=",
+            "messageId": "2754332662377047",
+            "message_id": "2754332662377047",
+            "publishTime": "2021-08-01T13:48:51.299Z",
+            "publish_time": "2021-08-01T13:48:51.299Z",
+        },
+        "subscription": "projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub",
+    }
     req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
 
     assert main.main_cards(data) == "200"
 
 
-@mock.patch("main._parse_request", return_value = ({
-    "eventType": "cards",
-    "eventSubType": "reregister",
-    "operation": "update",
-    "eventDetails": {
-      "source": {
-        "code": 1,
-        "name": "CPORTAL"
-      },
-      "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
-      "publishedAt": "2018-11-11T11:01:59+11:11",
-      "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
-      "profile": {
-        "crn": "3e24ab9f3a9e98ae8f277b5258553bdf",
-        "crnHash": "26249410976150692bb2056107ba6eb62c6d8f3746ffeb38cf374f3a3de9b1b8",
-        "account": {
-          "accountType": {
-            "code": 1002,
-            "name": "EDR"
-          },
-          "cardNumber": "ca4b628a53f04230642ee13ed48ca80e",
-          "cards": {
-            "newCardNumber": "ca4b628a53f04230642ee13ed48ca80e-reregister"  + datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
-          }
-        }
-      }
-    }
-},1))
+@mock.patch(
+    "main._parse_request",
+    return_value=(
+        {
+            "eventType": "cards",
+            "eventSubType": "reregister",
+            "operation": "update",
+            "eventDetails": {
+                "source": {"code": 1, "name": "CPORTAL"},
+                "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
+                "publishedAt": "2018-11-11T11:01:59+11:11",
+                "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
+                "profile": {
+                    "crn": "c78a09b2d48445e0bc7a233257984fdb",
+                    "crnHash": "53e23eb30f9f9d55da90d3836fde4599acff88860cc2c97819bc5e5bf350089f",
+                    "account": {
+                        "accountType": {"code": 1002, "name": "EDR"},
+                        "cardNumber": "476161f03910f2bc459c9d98c8508d4c",
+                        "cards": {
+                            "newCardNumber": "476161f03910f2bc459c9d98c8508d4c-reregister"
+                            + datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
+                        },
+                    },
+                },
+            },
+        },
+        1,
+    ),
+)
 def test_main_cards_reregister(mock_parse_request):
-    data = {'deliveryAttempt': 1, 'message': {'data': 'eyJldmVudFR5cGUiOiAicHJlZmVyZW5jZXMiLCAiZXZlbnRTdWJUeXBlIjogInJlZGVtcHRpb24iLCAib3BlcmF0aW9uIjogInVwZGF0ZSIsICJldmVudERldGFpbHMiOiB7InNvdXJjZSI6IHsiY29kZSI6IDEsICJuYW1lIjogIkNQT1JUQUwifSwgInRyYWNraW5nSWQiOiAiMWI2NzFhNjQtNDBkNS00OTFlLTk5YjAtZGEwMWZmMWYzMzQxIiwgInB1Ymxpc2hlZEF0IjogIjIwMTgtMTEtMTFUMTE6MDE6NTkrMTE6MTEiLCAiY29ycmVsYXRpb25JZCI6ICJhOGVlNmE5MC1jY2JjLTQ2NzgtODIzMC1hNGE2NWZiZjcwMDQiLCAicHJvZmlsZSI6IHsiY3JuIjogIjMzMDAwMDAwMDAwMzQ1MzU3NDgiLCAiY3JuSGFzaCI6ICI3YTMwMzk4ZDNlMTFiZmVjYjdjOWU3YjAxNGFkZmdkZmc2NDYzYzc2OGNjMzViOTQyZDZlYzQ0YWY2NmYxODUiLCAiYWNjb3VudCI6IHsiYWNjb3VudFR5cGUiOiB7ImNvZGUiOiAxMDAyLCAibmFtZSI6ICJFRFIifSwgImNhcmROdW1iZXIiOiAiOTM1NTA0OTM3OTMyOSIsICJwcmVmZXJlbmNlcyI6IFt7ImlkIjogMTAyNCwgIm5hbWUiOiAiVW5zdWJzY3JpYmUgQWxsIiwgInZhbHVlIjogIlFGRiJ9XX19fX0=', 'messageId': '2754332662377047', 'message_id': '2754332662377047', 'publishTime': '2021-08-01T13:48:51.299Z', 'publish_time': '2021-08-01T13:48:51.299Z'}, 'subscription': 'projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub'}
+    data = {
+        "deliveryAttempt": 1,
+        "message": {},
+        "subscription": "projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub",
+    }
     req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
 
     assert main.main_cards(data) == "200"
 
 
-@mock.patch("main._parse_request", return_value = ({
-    "eventType": "cards",
-    "eventSubType": "deregister",
-    "operation": "update",
-    "eventDetails": {
-      "source": {
-        "code": 1,
-        "name": "CPORTAL"
-      },
-      "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
-      "publishedAt": "2018-11-11T11:01:59+11:11",
-      "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
-      "profile": {
-        "crn": "c4f9ddd39c7a040041a069629832bc4f",
-        "crnHash": "b6e183926244b30462f71f26cd9391fbd1585af0a2efca7d493c94025b0bc527",
-        "account": {
-          "accountType": {
-            "code": 1002,
-            "name": "EDR"
-          },
-          "cardNumber": "9079c89e6426ab8fbe7303da47bd09e1",
-          "cards": {
-            "deregisteredCardNumber": "9079c89e6426ab8fbe7303da47bd09e1"
-        }
-      }
-    }
-  }},1))
+@mock.patch(
+    "main._parse_request",
+    return_value=(
+        {
+            "eventType": "cards",
+            "eventSubType": "deregister",
+            "operation": "update",
+            "eventDetails": {
+                "source": {"code": 1, "name": "CPORTAL"},
+                "trackingId": "1b671a64-40d5-491e-99b0-da01ff1f3341",
+                "publishedAt": "2018-11-11T11:01:59+11:11",
+                "correlationId": "a8ee6a90-ccbc-4678-8230-a4a65fbf7004",
+                "profile": {
+                    "crn": "b9c82ef7bf55b08a609ed89f6b20bb4f",
+                    "crnHash": "1d40285eb9758aad6d18a449d6602af710c428b753f5f3c3ce38adad284b4f99",
+                    "account": {
+                        "accountType": {"code": 1002, "name": "EDR"},
+                        "cardNumber": "5d7e27763c60c85cc4acfc8644d1257d",
+                        "cards": {
+                            "deregisteredCardNumber": "5d7e27763c60c85cc4acfc8644d1257d"
+                        },
+                    },
+                },
+            },
+        },
+        1,
+    ),
+)
 def test_main_cards_deregister(mock_parse_request):
-    data = {'deliveryAttempt': 1, 'message': {'data': 'eyJldmVudFR5cGUiOiAicHJlZmVyZW5jZXMiLCAiZXZlbnRTdWJUeXBlIjogInJlZGVtcHRpb24iLCAib3BlcmF0aW9uIjogInVwZGF0ZSIsICJldmVudERldGFpbHMiOiB7InNvdXJjZSI6IHsiY29kZSI6IDEsICJuYW1lIjogIkNQT1JUQUwifSwgInRyYWNraW5nSWQiOiAiMWI2NzFhNjQtNDBkNS00OTFlLTk5YjAtZGEwMWZmMWYzMzQxIiwgInB1Ymxpc2hlZEF0IjogIjIwMTgtMTEtMTFUMTE6MDE6NTkrMTE6MTEiLCAiY29ycmVsYXRpb25JZCI6ICJhOGVlNmE5MC1jY2JjLTQ2NzgtODIzMC1hNGE2NWZiZjcwMDQiLCAicHJvZmlsZSI6IHsiY3JuIjogIjMzMDAwMDAwMDAwMzQ1MzU3NDgiLCAiY3JuSGFzaCI6ICI3YTMwMzk4ZDNlMTFiZmVjYjdjOWU3YjAxNGFkZmdkZmc2NDYzYzc2OGNjMzViOTQyZDZlYzQ0YWY2NmYxODUiLCAiYWNjb3VudCI6IHsiYWNjb3VudFR5cGUiOiB7ImNvZGUiOiAxMDAyLCAibmFtZSI6ICJFRFIifSwgImNhcmROdW1iZXIiOiAiOTM1NTA0OTM3OTMyOSIsICJwcmVmZXJlbmNlcyI6IFt7ImlkIjogMTAyNCwgIm5hbWUiOiAiVW5zdWJzY3JpYmUgQWxsIiwgInZhbHVlIjogIlFGRiJ9XX19fX0=', 'messageId': '2754332662377047', 'message_id': '2754332662377047', 'publishTime': '2021-08-01T13:48:51.299Z', 'publish_time': '2021-08-01T13:48:51.299Z'}, 'subscription': 'projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub'}
+    data = {
+        "deliveryAttempt": 1,
+        "message": {},
+        "subscription": "projects/gcp-wow-rwds-etl-dev/subscriptions/data-dev-p24-partner-loyalty-api-sub",
+    }
     req = mock.Mock(get_json=mock.Mock(return_value=data), args=data)
 
     assert main.main_cards(data) == "200"
